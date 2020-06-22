e9b63b3b3e5cb16f308ffe1410dd8265c5a5bc99
Marcelo Muñoz <leonardo.munoz@dinardap.gob.ec>
2020-06-22 10:24:46 -0500

    Nueva semilla para seguridades

--- src/main/java/ec/gob/dinardap/autorizacion/constante/SemillaEnum.java
+++ src/main/java/ec/gob/dinardap/autorizacion/constante/SemillaEnum.java
@@ -1,7 +1,8 @@
 package ec.gob.dinardap.autorizacion.constante;
 
 public enum SemillaEnum {
-	SEMILLA_REMANENTE("ghs%6f.D");
+	SEMILLA_REMANENTE("ghs%6f.D"),
+	SEMILLA_SEGURIDAD("C0l!bX!3$#V0l@Nd%");
 	
 	private String semilla;
 	
